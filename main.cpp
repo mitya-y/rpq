8cc760bff4464ffc1b017bb9671342f189857a38
Mitya <8863314@gmail.com>
2024-10-27 03:03:46 +0300

    Added new test, add config struct. Updated algorithm (clear next frontier and add it with save old value), start write a mask applying (first try)

--- main.cpp
+++ main.cpp
@@ -9,6 +9,54 @@
 
 #include <fast_matrix_market/fast_matrix_market.hpp>
 
+// bad temporary algorithm
+static void invert_matrix(cuBool_Matrix mask) {
+  // invert mask
+  cuBool_Index nvals;
+  cuBool_Matrix_Nvals(mask, &nvals);
+
+  std::vector<cuBool_Index> rows(nvals), cols(nvals);
+  cuBool_Matrix_ExtractPairs(mask, rows.data(), cols.data(), &nvals);
+
+  cuBool_Index ncols, nrows;
+  cuBool_Matrix_Ncols(mask, &ncols);
+  cuBool_Matrix_Nrows(mask, &nrows);
+  std::vector inverted_mask(nrows, std::vector(ncols, true));
+
+  for (int i = 0; i < nvals; i++) {
+    inverted_mask[rows[i]][cols[i]] = false;
+  }
+
+  rows.clear();
+  rows.reserve(ncols * nvals - nvals);
+  cols.clear();
+  cols.reserve(ncols * nvals - nvals);
+  for (cuBool_Index i = 0; i < nrows; i++) {
+    for (cuBool_Index j = 0; j < ncols; j++) {
+      if (inverted_mask[i][j]) {
+        rows.push_back(i);
+        cols.push_back(j);
+      }
+    }
+  }
+
+  cuBool_Matrix_Build(mask, rows.data(), cols.data(), rows.size(), CUBOOL_HINT_NO);
+}
+
+static void apply_not_mask(cuBool_Matrix matrix, cuBool_Matrix mask) {
+  cuBool_Matrix inverted_mask;
+  cuBool_Matrix_Duplicate(mask, &inverted_mask);
+  invert_matrix(inverted_mask);
+
+  cuBool_Matrix tmp_frontier;
+  cuBool_Matrix_Duplicate(matrix, &tmp_frontier);
+
+  cuBool_Matrix_EWiseMult(matrix, tmp_frontier, inverted_mask, CUBOOL_HINT_NO);
+
+  cuBool_Matrix_Free(inverted_mask);
+  cuBool_Matrix_Free(tmp_frontier);
+}
+
 cuBool_Matrix regular_path_query(
     // vector of sparse graph matrices for each label
     const std::vector<cuBool_Matrix> &graph,
@@ -18,8 +66,10 @@ cuBool_Matrix regular_path_query(
     const std::vector<cuBool_Matrix> &automat,
     const std::vector<cuBool_Index> &start_states
     ) {
+  cuBool_Status status;
+
   // this is pointers to normal matrix.
-  cuBool_Matrix frontier {}, symbol_frontier {}, next_frontier {}; 
+  cuBool_Matrix frontier {}, symbol_frontier {}, next_frontier {};
 
   cuBool_Index graph_nodes_number = 0;
   cuBool_Index automat_nodes_number = 0;
@@ -28,6 +78,7 @@ cuBool_Matrix regular_path_query(
   std::vector<cuBool_Matrix> graph_transpsed;
   graph_transpsed.reserve(graph.size());
   for (auto label_matrix : graph) {
+    // TODO call new
     graph_transpsed.emplace_back();
     cuBool_Matrix_Transpose(graph_transpsed.back(), label_matrix, CUBOOL_HINT_NO);
   }
@@ -36,7 +87,12 @@ cuBool_Matrix regular_path_query(
   std::vector<cuBool_Matrix> automat_transpsed;
   automat_transpsed.reserve(graph.size());
   for (auto label_matrix : automat) {
+    cuBool_Index nrows, ncols;
+    cuBool_Matrix_Nrows(label_matrix, &nrows);
+    cuBool_Matrix_Ncols(label_matrix, &ncols);
+
     automat_transpsed.emplace_back();
+    cuBool_Matrix_New(&automat_transpsed.back(), nrows, ncols);
     cuBool_Matrix_Transpose(automat_transpsed.back(), label_matrix, CUBOOL_HINT_NO);
   }
 
@@ -76,24 +132,45 @@ cuBool_Matrix regular_path_query(
 
   cuBool_Index states = source_vertices.size();
 
-  const auto label_number = std::max(graph.size(), automat.size());
+  const auto label_number = std::min(graph.size(), automat.size());
   while (states > 0) {
     std::swap(frontier, next_frontier);
 
-    // TODO: clear next_frontier?
+    uint32_t nvals;
+    cuBool_Matrix_Nvals(reacheble, &nvals);
+    std::cout << "nvals = " << nvals << " states = " << states << std::endl;
+
+    // clear next_frontier
+    cuBool_Matrix_Build(next_frontier, nullptr, nullptr, 0, CUBOOL_HINT_NO);
 
     for (int i = 0; i < label_number; i++) {
       if (graph[i] == nullptr || automat[i] == nullptr) {
         continue;
       }
-      cuBool_Matrix_EWiseMult(symbol_frontier, automat_transpsed[i], frontier, CUBOOL_HINT_NO);
-      // this must be mult with mask: next_frontier = (symbol_frontier * graph[i]) & reacheble
-      cuBool_Matrix_EWiseMult(next_frontier, symbol_frontier, graph[i], CUBOOL_HINT_NO);
+
+      // is this overwrite symbol_frontier? not :(
+      status = cuBool_MxM(symbol_frontier, automat_transpsed[i], frontier, CUBOOL_HINT_NO);
+      assert(status == CUBOOL_STATUS_SUCCESS);
+
+      // this must be mult with mask: next_frontier += (symbol_frontier * graph[i]) & (!reachible)
+      status = cuBool_MxM(next_frontier, symbol_frontier, graph[i], CUBOOL_HINT_ACCUMULATE);
+      assert(status == CUBOOL_STATUS_SUCCESS);
+
+      // apply mask
+      apply_not_mask(next_frontier, reacheble);
+
+      cuBool_Matrix_Nvals(symbol_frontier, &nvals);
+      std::cout << "nvals symbol_frontier = " << nvals << std::endl;
+      cuBool_Matrix_Nvals(next_frontier, &nvals);
+      std::cout << "nvals next_frontier = " << nvals << std::endl;
     }
 
-    // this must be assign with matrix: reacheble = next_frontier & reacheble
-    // also maybe here leak old reacheble memory
-    cuBool_Matrix_Duplicate(next_frontier, &reacheble);
+    // this must be accumulate with mask and save old value: reacheble += next_frontier & reacheble
+    cuBool_Matrix tmp_reacheble;
+    cuBool_Matrix_Duplicate(reacheble, &tmp_reacheble);
+    cuBool_Matrix_EWiseAdd(reacheble, tmp_reacheble, next_frontier, CUBOOL_HINT_NO);
+    cuBool_Matrix_Free(tmp_reacheble);
+
     cuBool_Matrix_Nvals(next_frontier, &states);
   }
 
@@ -104,7 +181,14 @@ cuBool_Matrix regular_path_query(
   return reacheble;
 }
 
-void test() {
+struct Config {
+  std::vector<std::string> graph_data;
+  std::vector<std::string> automat_data;
+  std::string_view meta_data;
+  std::string_view sources_data;
+};
+
+void test(const Config &config) {
   cuBool_Initialize(CUBOOL_HINT_NO);
 
   std::vector<cuBool_Matrix> graph;
@@ -112,18 +196,13 @@ void test() {
   std::vector<cuBool_Matrix> automat;
   std::vector<cuBool_Index> start_states;
 
-  std::vector<std::string> graph_data { "test_data/a.mtx", "test_data/b.mtx" };
-  std::vector<std::string> automat_data { "test_data/1_a.mtx", "" };
-  std::string_view meta_data("test_data/1_meta.txt");
-  std::string_view sources_data("test_data/1_sources.txt");
-
   int64_t nrows = 0, ncols = 0;
   std::vector<cuBool_Index> rows, cols;
   std::vector<bool> vals;
 
   // load graph
-  graph.reserve(graph_data.size());
-  for (const auto &data : graph_data) {
+  graph.reserve(config.graph_data.size());
+  for (const auto &data : config.graph_data) {
     graph.emplace_back();
     if (data.empty()) {
       continue;
@@ -140,8 +219,8 @@ void test() {
   }
 
   // load automat
-  automat.reserve(automat_data.size());
-  for (const auto &data : automat_data) {
+  automat.reserve(config.automat_data.size());
+  for (const auto &data : config.automat_data) {
     automat.emplace_back();
     if (data.empty()) {
       continue;
@@ -163,11 +242,31 @@ void test() {
 
   auto answer = regular_path_query(graph, source_vertices, automat, start_states);
 
+  uint32_t n;
+  cuBool_Matrix_Nvals(answer, &n);
+  std::cout << n << std::endl;
+  // cuBool_Matrix_Nrows(answer, &n);
+  // std::cout << n << std::endl;
+  // cuBool_Matrix_Ncols(answer, &n);
+  // std::cout << n << std::endl;
+
   cuBool_Matrix_Free(answer);
 
   cuBool_Finalize();
 }
 
 int main() {
-  test();
+  Config config {
+    .graph_data = { "test_data/a.mtx", "test_data/b.mtx" },
+    .automat_data { "test_data/1_a.mtx", "" },
+    .meta_data = "test_data/1_meta.txt",
+    .sources_data = "test_data/1_sources.txt",
+  };
+
+  config = Config {
+    .graph_data = { "test_data/example/graph_a.mtx", "test_data/example/graph_b.mtx" },
+    .automat_data = { "test_data/example/automat_a.mtx", "test_data/example/automat_b.mtx" },
+  };
+
+  test(config);
 }
