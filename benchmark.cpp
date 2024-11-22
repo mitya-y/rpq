8eb755ea0e24554cdd1048083f7ce6773b4cf76e
Mitya <8863314@gmail.com>
2024-11-22 13:34:37 +0300

    load data for each query, but it is too slow

--- benchmark.cpp
+++ benchmark.cpp
@@ -25,19 +25,14 @@
 struct Query {
   cuBool_Index source = 0, dest = 0;
 
+  std::vector<cuBool_Matrix> graph;
+  std::vector<cuBool_Matrix> automat;
+
   std::vector<uint32_t> labels;
   std::vector<bool> inverse_lables;
 
   std::vector<cuBool_Index> sourece_vertices;
   std::vector<cuBool_Index> start_states;
-};
-
-struct Wikidata {
-  std::vector<cuBool_Matrix> graph;
-
-  std::vector<cuBool_Matrix> automat;
-
-  std::vector<Query> queries;
 };
 
 static bool load_matrix(cuBool_Matrix *matrix, std::string_view filename) {
@@ -57,94 +52,104 @@ static bool load_matrix(cuBool_Matrix *matrix, std::string_view filename) {
   return true;
 }
 
-static void load_wikidata(Wikidata &data) {
-  data.graph.assign(PROP_COUNT + 1, nullptr);
-  data.automat.assign(PROP_COUNT + 1, nullptr);
+static bool load_query(Query &query, uint32_t query_number) {
+  std::string filename = std::format("{}{}/meta.txt", QUERIES_DIR, query_number);
+  std::ifstream query_file(filename);
+  if (not query_file) {
+    std::cout << "skipped query " << query_number << ", file not exists\n";
+    return false;
+  }
 
-  data.queries.reserve(QUERY_COUNT);
+  query_file >> query.source >> query.dest;
+  if (query.source == 0 && query.dest == 0) {
+    std::cout << "skipped query " << query_number << ", sourse and dest is 0\n";
+    return false;
+  }
+  query.source--;
+  query.dest--;
 
-  for (int query_number = 1; query_number <= QUERY_COUNT; query_number++) {
-    std::string filename = std::format("{}{}/meta.txt", QUERIES_DIR, query_number);
-    std::cout << filename << std::endl;
-    std::ifstream query_file(filename);
-    if (not query_file) {
-      std::cout << "skipped query " << query_number << ", file not exists\n";
-      continue;
+  uint32_t src_verts_number = 0;
+  query_file >> src_verts_number;
+  query.sourece_vertices.resize(src_verts_number);
+  for (auto &vert : query.sourece_vertices) {
+    query_file >> vert;
+    vert--;
+  }
+
+  uint32_t start_states_number = 0;
+  query_file >> start_states_number;
+  query.start_states.resize(start_states_number);
+  for (auto &state : query.start_states) {
+    query_file >> state;
+    state--;
+  }
+
+  uint32_t labels_number = 0;
+  query_file >> labels_number;
+  query.labels.resize(labels_number);
+  query.inverse_lables.resize(labels_number);
+  for (int i = 0; i < labels_number; i++) {
+    int label;
+    query_file >> label;
+    query.labels[i] = std::abs(label);
+    query.inverse_lables[i] = label < 0;
+  }
+
+  query.graph.assign(labels_number, nullptr);
+  query.automat.assign(labels_number, nullptr);
+
+  for (int i = 0; i < labels_number; i++) {
+    uint32_t label = query.labels[i];
+
+    std::string filename = std::format("{}{}.txt", WIKIDATA_DIR, label);
+    if (not load_matrix(&query.graph[i], filename)) {
+      throw std::runtime_error(("can not find matrix for label " +
+                                std::to_string(label)).c_str());
+      return false;
     }
 
-    data.queries.emplace_back();
-    auto &query = data.queries.back();
-
-    query_file >> query.source >> query.dest;
-    if (query.source == 0 && query.dest == 0) {
-      std::cout << "skipped query " << query_number << ", sourse and dest is 0\n";
-      continue;
-    }
-    query.source--;
-    query.dest--;
-
-    uint32_t src_verts_number = 0;
-    query_file >> src_verts_number;
-    query.sourece_vertices.resize(src_verts_number);
-    for (auto &vert : query.sourece_vertices) {
-      query_file >> vert;
-      vert--;
-    }
-
-    uint32_t start_states_number = 0;
-    query_file >> start_states_number;
-    query.start_states.resize(start_states_number);
-    for (auto &state : query.start_states) {
-      query_file >> state;
-      state--;
-    }
-
-    uint32_t labels_number = 0;
-    query_file >> labels_number;
-    query.labels.resize(labels_number);
-    query.inverse_lables.resize(labels_number);
-    for (int i = 0; i < labels_number; i++) {
-      int label;
-      query_file >> label;
-      query.labels[i] = std::abs(label);
-      query.inverse_lables[i] = label < 0;
-    }
-
-
-    for (int i = 0; i < labels_number; i++) {
-      uint32_t label = query.labels[i];
-
-      if (data.graph[label] == nullptr) {
-        std::string filename = std::format("{}{}.txt", WIKIDATA_DIR, label);
-        if (not load_matrix(&data.graph[label], filename)) {
-          throw std::runtime_error(("can not find matrix for label " +
-                                    std::to_string(label)).c_str());
-        }
-      }
-
-      std::string filename = std::format("{}{}/{}.txt", QUERIES_DIR, query_number,
-        query.inverse_lables[i] ? -(int)label : (int)label);
-      if (not load_matrix(&data.automat[label], filename)) {
-        throw std::runtime_error(("can not find matrix for label " +
-                                  std::to_string(label)).c_str());
-      }
+    filename = std::format("{}{}/{}.txt", QUERIES_DIR, query_number,
+      query.inverse_lables[i] ? -(int)label : (int)label);
+    if (not load_matrix(&query.automat[i], filename)) {
+      throw std::runtime_error(("can not find matrix for label " +
+                                std::to_string(label)).c_str());
+      return false;
     }
   }
+
+  return true;
 }
+
+static void clear_query(Query &query) {
+  for (auto matrix : query.graph) {
+    cuBool_Matrix_Free(matrix);
+  }
+
+  for (auto matrix : query.automat) {
+    cuBool_Matrix_Free(matrix);
+  }
+}
+
+static void make_query(const Query &query) {
+  auto recheable = regular_path_query(query.graph, query.sourece_vertices,
+                                      query.automat, query.start_states);
+
+  cuBool_Matrix_Free(recheable);
+}
+
 
 bool benchmark() {
   cuBool_Initialize(CUBOOL_HINT_NO);
 
   struct timespec start, finish;
 
-  // preload
-  printf("Loading the matrices...\n");
-  Wikidata wikidata;
-  load_wikidata(wikidata);
-  // TODO: calculate memory usage
-  printf("Loading done!\n");
+  for (uint32_t query_number = 1; query_number <= QUERY_COUNT; query_number++) {
+    std::cout << "query number " << query_number << std::endl;
 
-  getchar();
+    Query query;
+    load_query(query, query_number);
+    clear_query(query);
+  }
 
   return true;
 }
